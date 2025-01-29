f28c86132c4aa7df3b81042449f36a0fc5cdf8cd
Marcio Carneiro <marcioscar@gmail.com>
2025-01-29 09:13:24 -0300

    outra

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ import pandas as pd
 import locale
 import plotly.express as px
 import calendar
-from streamlit_extras.metric_cards import style_metric_cards
+
 
 locale.setlocale(locale.LC_ALL, 'pt_BR.UTF-8')
 
